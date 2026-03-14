d51752f2bc6af8de8122c16f7c0c82b9278065b5
Veeresh Kakamari <veereshkakamri@gmail.com>
2026-03-14 20:11:30 +0530

    fix image case issue for deployment

--- src/landing_page/about/Team.js
+++ src/landing_page/about/Team.js
@@ -12,7 +12,7 @@ function Team() {
       >
         <div className="col-6 p-5 text-center">
           <img
-            src="/media/images/Mypic.jpg"
+            src="public/media/images/Mypic.JPG"
             alt="Developer"
             style={{ borderRadius: "100%", width: "70%" }}
           ></img>
